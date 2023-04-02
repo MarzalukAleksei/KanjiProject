0b77f3e4c7d4b6c5b5249dc9d0332e88f00db757
BlackOokami <aramarza@mail.ru>
2023-04-02 17:20:41 +0300

    Initial Commit

---
+++ KanjiProject/KanjiProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  KanjiProjectApp.swift
+//  KanjiProject
+//
+//  Created by ブラック狼 on 2023/04/02.
+//
+
+import SwiftUI
+
+@main
+struct KanjiProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
