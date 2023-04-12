4a5f014d13f415421c0be8cb1f37c714ea517f09
BlackOokami <aramarza@mail.ru>
2023-04-13 01:34:43 +0300

    scene delegate

--- KanjiProject/KanjiProjectApp.swift
+++ KanjiProject/KanjiProjectApp.swift
@@ -6,10 +6,20 @@
 //
 
 import SwiftUI
+//import UIKit
+//
+//class UIAppDelegate: NSObject, UIApplicationDelegate {
+//    func application(_ application: UIApplication, willFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+//
+//        return true
+//    }
+//}
 
 @main
 struct KanjiProjectApp: App {
-//    let array: [KanjiModel] = KanjiMapper().gettingData(entity: FileMapper().transform(data: try! FileManager().loadFile(fileName: "Kanji", fileType: .csv)))
+    
+//    @UIApplicationDelegateAdaptor(UIAppDelegate.self) var appDelegate
+    
     let stores = Stores()
     
     var body: some Scene {
